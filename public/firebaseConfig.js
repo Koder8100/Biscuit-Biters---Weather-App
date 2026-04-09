aed6f75fe414e6c1f17391230825a46c0445d8d8
Kevin Sammy <sammykev.wrk8100@gmail.com>
2026-04-08 20:05:03 -0400

    Firebase set up, updated all files, created new files

---
+++ public/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC3qgxLNQVCjOyLfkCtqglcDiytDjHFWPs",
+  authDomain: "weathersmart--weatherapp.firebaseapp.com",
+  projectId: "weathersmart--weatherapp",
+  storageBucket: "weathersmart--weatherapp.firebasestorage.app",
+  messagingSenderId: "621528182951",
+  appId: "1:621528182951:web:ea7f13509064f728bd2b82",
+  measurementId: "G-YYF443QGMQ"
+};
+
+export default firebaseConfig;
